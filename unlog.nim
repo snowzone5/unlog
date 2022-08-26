eaa641ed0e277f3819504a43be2642c41448e3a9
Tony Summerfelt <96543080+snowzone5@users.noreply.github.com>
2022-08-26 13:44:59 -0400

    issue9, added caller argument (#11)

--- unlog.nim
+++ unlog.nim
@@ -8,11 +8,13 @@ import chronicles
 import std/logging
 import morelogging
 import strutils
+#import std/distros
 
 var debug: bool = false
 var ver: string = "Unlog v0.05\n" 
 var logfile: string = "unlog.log"
 var loglevel: string = "INFO"
+var caller: string = "main"
 var lineNumber: string = "0"
 var msg: string = ""
 var paramCount = paramCount()
@@ -27,7 +29,7 @@ proc version =
       quit(QuitSuccess)
 
 proc help =
-      echo "Usage: unlog [-h | --help] [ -v | --version ] --logstyle= [ sl | mul | nl | jctl | mxl ] --log=\"<filename.log>\" --loglevel=[ INFO | DEBUG | WARN | ERROR | FATAL ] [ --ln=# | --linenumber=# ] --msg=\"Log message.\" [-x=var1 [-x=var2 ... ]"
+      echo "Usage: unlog [-h | --help] [ -v | --version ] --logstyle= [ sl | mul | nl | jctl | mxl ] --log=\"<filename.log>\" --loglevel=[ INFO | DEBUG | WARN | ERROR | FATAL ] [ -c= | --caller=\"proc\" ] [ --ln=# | --linenumber=# ] --msg=\"Log message.\" [-x=var1 [-x=var2 ... ]"
       echo ""
       echo "-h or --help"
       echo "        Print this help."
@@ -49,6 +51,9 @@ proc help =
       echo "--loglevel"
       echo "        INFO, DEBUG, WARN, ERROR, FATAL. If omitted, default is INFO."
       echo ""
+      echo "-c or --caller"
+      echo "        Calling function/proc/class/method. By itself string will default to \"main\" "
+      echo ""
       echo "--ln or --linenumber"
       echo "        Line number of caller. Internally this is actually a string. This may change in the future."
       echo ""
@@ -66,35 +71,34 @@ proc multiline =
         var success = defaultChroniclesStream.output.open(logfile, fmAppend)
 
       case loglevel
-      of "INFO":   info  "", lineNumber, msg, extraArgs
-      of "NOTICE": notice  "", lineNumber, msg, extraArgs
-      of "DEBUG":  debug  "", lineNumber, msg, extraArgs
-      of "WARN":   warn  "", lineNumber, msg, extraArgs
-      of "ERROR":  error  "", lineNumber, msg, extraArgs
-      of "FATAL":  fatal  "", lineNumber, msg, extraArgs
+      of "INFO":   info  "", caller, lineNumber, msg, extraArgs
+      of "NOTICE": notice  "", caller, lineNumber, msg, extraArgs
+      of "DEBUG":  debug  "", caller, lineNumber, msg, extraArgs
+      of "WARN":   warn  "", caller, lineNumber, msg, extraArgs
+      of "ERROR":  error  "", caller, lineNumber, msg, extraArgs
+      of "FATAL":  fatal  "", caller, lineNumber, msg, extraArgs
 
       quit(QuitSuccess)
 
 proc stdoutlogging =
       case loglevel:
-      of "INFO": slog.info(loglevel, ": ", msg," -> ",  extraArgs)
-      of "NOTICE": slog.info(loglevel, ": ", msg," -> ",  extraArgs)
-      of "DEBUG": slog.info(loglevel, ": ", msg," -> ",  extraArgs)
-      of "WARN": slog.info(loglevel, ": ", msg," -> ",  extraArgs)
-      of "ERROR": slog.info(loglevel, ": ", msg," -> ",  extraArgs)
-      of "FATAL": slog.info(loglevel, ": ", msg," -> ",  extraArgs)
+      of "INFO": slog.info(loglevel, ":", caller, ":", lineNumber, ": ", msg," -> ",  extraArgs)
+      of "NOTICE": slog.info(loglevel, ": ", caller, ":", lineNumber, ": ", msg," -> ",  extraArgs)
+      of "DEBUG": slog.info(loglevel, ": ", caller, ":", lineNumber, ": ",msg," -> ",  extraArgs)
+      of "WARN": slog.info(loglevel, ": ", caller, ":", lineNumber, ": ",msg," -> ",  extraArgs)
+      of "ERROR": slog.info(loglevel, ": ", caller, ":", lineNumber, ": ", msg," -> ",  extraArgs)
+      of "FATAL": slog.info(loglevel, ": ", caller, ":", lineNumber, ": ", msg," -> ",  extraArgs)
 
       echo ""
 
       quit(QuitSuccess)
 
 proc usenimlogger =
-        var consoleLog = newConsoleLogger(fmtStr="[$date $time] - $levelname: ")          
-        var fileLog = newFileLogger(logfile, fmAppend,  fmtStr="[$date $time] - $levelname: ")
+        var consoleLog = newConsoleLogger(fmtStr="[$date $time] - $levelname:")          
+        var fileLog = newFileLogger(logfile, fmAppend,  fmtStr="[$date $time] - $levelname:")
 
         addHandler(consoleLog)
         addHandler(fileLog)
-
 
 proc checkargs =
         var argCounter : int
@@ -104,8 +108,9 @@ proc checkargs =
             help()
 
         for p in 1 .. paramCount:
-            #% echo "\nparam ", p, ": ", paramStr(p)
-            #% echo ""
+            if debug:
+                echo "\nparam ", p, ": ", paramStr(p)
+                echo ""
 
             for kind, key, value in getOpt():
                 case kind
@@ -115,17 +120,24 @@ proc checkargs =
               
                 of cmdLongOption, cmdShortOption:
                     case key
+                    of "d": debug=true
                     of "log": logfile=value
                     of "loglevel": loglevel=value
                     of "ln": lineNumber=value
                     of "linenumber": lineNumber=value
+                    of "c": 
+                          if value != "":
+                              caller=value
+                    of "caller": 
+                               if value != "":
+                                   caller=value
                     of "msg": msg=value
                     of "h": help()
                     of "help": help()
                     of "v": version()
                     of "version": version()
                     of "x":
-                          if extraCounter < ((paramCount-1)-extraCounter):
+                          if extraCounter < ((paramCount-2)-extraCounter):
                               extraArgs.add(value & " ")
                               extraCounter.inc
                     of "mul": logStyle = "multiLine"
@@ -140,10 +152,10 @@ proc checkargs =
             
                     else:
                       echo "Unknown option: ", key
+                      help()
             
                 of cmdEnd: # matches: of cmdLongOption, cmdShortOption:
                     discard
-
 
 checkargs() 
 
@@ -153,17 +165,17 @@ of "stdout": stdoutlogging()
 of "nimlogger": 
               usenimlogger()
               case loglevel:
-              of "INFO":  log(lvlInfo, "ln:",lineNumber, " - ", msg, " -> ", extraArgs )
-              of "NOTICE":  log(lvlNotice, "ln:",lineNumber, " - ", msg, " -> ", extraArgs )
-              of "DEBUG":  log(lvlDebug, "ln:",lineNumber, " - ", msg, " -> ", extraArgs )
-              of "WARN":  log(lvlWarn, "ln:",lineNumber, " - ", msg, " -> ", extraArgs )
-              of "ERROR":  log(lvlError, "ln:",lineNumber, "- ", msg, " -> ", extraArgs )
-              of "FATAL":  log(lvlFatal, "ln:",lineNumber, " - ", msg," -> ",  extraArgs )
+              of "INFO":  log(lvlInfo, caller, ":", lineNumber, " - ", msg, " -> ", extraArgs )
+              of "NOTICE":  log(lvlNotice, caller, ":", lineNumber, " - ", msg, " -> ", extraArgs )
+              of "DEBUG":  log(lvlDebug, caller, ":", lineNumber, " - ", msg, " -> ", extraArgs )
+              of "WARN":  log(lvlWarn, caller, ":", lineNumber, " - ", msg, " -> ", extraArgs )
+              of "ERROR":  log(lvlError, caller, ":", lineNumber, "- ", msg, " -> ", extraArgs )
+              of "FATAL":  log(lvlFatal, caller, ":", lineNumber, " - ", msg," -> ",  extraArgs )
               echo ""
               quit(QuitSuccess)
 
-
 # TODO: add nim-morelogging (journald)
+# TODO: look into detecting os
 
 # TODO: add max/squish logging :)
 #CRITICAL = "! "
